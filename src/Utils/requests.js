38c49e32af6dedd1cab96a19eeb299f9aaf0e1ce
Jesús Montoya <gsus.montoya.17@gmail.com>
2019-09-30 20:28:46 -0500

    request and utils modified

--- src/Utils/requests.js
+++ src/Utils/requests.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const logIn = function (userLogin) {
-    return axios.post('logIn/', {userLogin});
+    return axios.post(`logIn/`, {userLogin});
 };
 
 const signIn = function (userSignIn) {
-    return axios.post('signIn/', userSignIn);
+    return axios.post(`signIn/`, userSignIn);
 };
 
 export {logIn, signIn};
