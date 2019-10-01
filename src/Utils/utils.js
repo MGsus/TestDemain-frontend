990ffd74da437e77595a19aadfb5ff86c290295d
Jesús Montoya <gsus.montoya.17@gmail.com>
2019-09-30 19:53:54 -0500

    env toString

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const configureRequests = () => {
-    axios.defaults.baseURL = process.env.BACKEND_URL;
+    axios.defaults.baseURL = process.env.BACKEND_URL.toString();
 };
 
 export default configureRequests;
